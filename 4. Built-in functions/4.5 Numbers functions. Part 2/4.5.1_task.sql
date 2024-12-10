71d9b73be94d117d7204f87e4c0bda3e8211a40f
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-12-10 21:17:25 +0400

    complited

---
+++ 4. Built-in functions/4.5 Numbers functions. Part 2/4.5.1_task.sql
@@ -0,0 +1,6 @@
+SELECT num,
+       CONV(num, 10, 2) AS bin,
+       CONV(num, 10, 8) AS oct,
+       CONV(num, 10, 16) AS hex
+FROM PrimeNumbers
+ORDER BY num DESC
---
+++ 4. Built-in functions/4.5 Numbers functions. Part 2/4.5.3_task.sql
@@ -0,0 +1,8 @@
+SELECT title, 
+       ROUND(
+             (flickmetrix + metacritic + imdb + letterboxd + kinopoisk 
+             - LEAST(flickmetrix, metacritic, imdb, letterboxd, kinopoisk) 
+             - GREATEST(flickmetrix, metacritic, imdb, letterboxd, kinopoisk)) / 3, 2
+             ) AS average_rating
+FROM Movies
+ORDER BY average_rating DESC, title ASC
