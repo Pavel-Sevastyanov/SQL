b2fe52d9114a3cb65d6cc7ee9d4e9e04bdf6ff75
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-12-26 19:51:33 +0400

    complited

---
+++ 5. Grouping data/5.3 Task solution/5.3.9_task.sql
@@ -0,0 +1,6 @@
+SELECT TRIM(LOWER(product_name)) AS product_name,
+       DATE_FORMAT(sale_date, '%Y-%m') AS sale_date,
+       COUNT(*) AS total
+FROM Sales
+GROUP BY TRIM(LOWER(product_name)), DATE_FORMAT(sale_date, '%Y-%m')
+ORDER BY product_name, sale_date
